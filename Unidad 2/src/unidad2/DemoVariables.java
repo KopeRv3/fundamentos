c847b0592ab8dd53c6da9c430419669c35c5da4d
KopeRv3 <112568854+KopeRv3@users.noreply.github.com>
2022-09-13 10:57:15 -0500

    Add files via upload

---
+++ Unidad 2/src/unidad2/DemoVariables.java
@@ -0,0 +1,42 @@
+package unidad2;
+
+public class DemoVariables {
+	
+	public static void main(String[] args) {
+		//declarar una variable de tipo enterolarga
+		long a;
+		a = 1250;
+		System.out.println(a);
+		a = -233372036854775808L;
+		System.out.println(a);
+		//declarar una variable de tipo enteroCorto
+		int b = 1250;
+		b = 2147483647;
+		System.out.println(b);
+		//declarar una variable de tipo short
+		short c = 32767;
+		System.out.println(c);
+		//declarar una variable tipo byte
+		byte d = 0;
+		System.out.println(d);
+		//declarar una variable tipo float
+		float e = 3;
+		System.out.println(e);
+		//definir una variable tipo double
+		double f = 5.18129;
+		System.out.println(f);
+		double g = 5.4e-2;
+		System.out.println(g);
+		int h,j,k;
+		float x,z;
+		byte x1 = 5, x2 = 10;
+		System.out.println(Math.sqrt(-1));
+		
+		System.out.println(1.1e200*1.1e200);
+		
+		System.out.println(-1.1e200*1.1e200);
+		
+
+	}
+
+}
